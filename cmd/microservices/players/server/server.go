5d8b14d9c2f5dcda7d907318e27749963f1f388b
mrubio7 <manuel.rubio.arias@gmail.com>
2024-10-17 21:20:26 +0200

    health endpoint in microservice

--- cmd/microservices/players/server/server.go
+++ cmd/microservices/players/server/server.go
@@ -8,6 +8,8 @@ import (
 	"ibercs/pkg/logger"
 	"ibercs/pkg/service"
 	pb "ibercs/proto/players"
+	"net/http"
+	"time"
 )
 
 type Server struct {
@@ -23,6 +25,15 @@ func New() *Server {
 	}
 	db := database.New(cfg.Database)
 	playerService := service.NewPlayersService(db)
+
+	go func() {
+		http.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte(time.Now().String()))
+		})
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			logger.Error(err.Error())
+		}
+	}()
 
 	return &Server{
 		PlayersService: playerService,
