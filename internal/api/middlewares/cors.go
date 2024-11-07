5f9a9ba6d845d9e647f0ff0ee8de44a3ca5a1977
mrubio7 <manuel.rubio.arias@gmail.com>
2024-11-07 21:53:31 +0100

    Methods cors

--- internal/api/middlewares/cors.go
+++ internal/api/middlewares/cors.go
@@ -21,7 +21,7 @@ func CORSMiddleware() gin.HandlerFunc {
 
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Cache-Control")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE, PUT, PATCH")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
